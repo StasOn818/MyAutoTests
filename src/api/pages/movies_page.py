8a0ad3458151bd501062d1156df97565bb8ab5f4
StasOn818 <kudrstason818@gmail.com>
2025-03-20 20:32:07 +0200

    add code

--- src/api/pages/movies_page.py
+++ src/api/pages/movies_page.py
@@ -5,7 +5,7 @@ class MoviesPage(BaseAPI):
     def get_popular_movies(self, page=1):
         return self._make_request("/movie/popular", params={"page": page})
 
-    # TEST
+    # TEST 3
 
     def get_movie_details(self, movie_id):
         return self._make_request(f"/movie/{movie_id}")
